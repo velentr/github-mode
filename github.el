34bb9631723ba9a62d8ef8ada0cb70e5c4aba8bd
Brian Kubisiak <brian@kubisiak.com>
2021-05-21 11:39:03 -0700

    pr: add buffer for displaying a pr summary
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- github.el
+++ github.el
@@ -56,38 +56,7 @@
      }
     }" gh-owner gh-repo))
 
-(defun gh-pr-query (number)
-  "Retrieve the graphql query for getting an overview of a specific pr NUMBER."
-  (format
-   "query {
-     repository(owner:\"%s\", name:\"%s\") {
-      pullRequest(number:%d) {
-       title
-       body
-       baseRef {
-        name
-        target {
-         oid
-        }
-       }
-       author {
-        login
-       }
-       files(first:100) {
-        edges {
-         node {
-          path
-          additions
-          deletions
-         }
-        }
-       }
-      }
-     }
-    }" gh-owner gh-repo number))
-
-
-(defun gh--parse-pr-query (query)
+(defun gh--parse-all-prs-query (query)
   "Parse the QUERY data from a graphql pr request."
   (let ((data (gethash "edges" (gethash "search" (gethash "data" query)))))
     (seq-map 'gh--parse-pr-queryline data)))
@@ -112,6 +81,56 @@
      (gh--in-merge-queue labels)
      (gh--pr-size labels)
      (gh--filter-labels labels))))
+
+(defun gh-pr-query (number)
+  "Retrieve the graphql query for getting an overview of a specific pr NUMBER."
+  (format
+   "query {
+     repository(owner:\"%s\", name:\"%s\") {
+      pullRequest(number:%d) {
+       title
+       body
+       headRefName
+       headRefOid
+       author {
+        login
+       }
+       files(first:100) {
+        edges {
+         node {
+          path
+          additions
+          deletions
+         }
+        }
+       }
+      }
+     }
+    }" gh-owner gh-repo number))
+
+(defun gh--parse-single-pr-query (query)
+  "Parse the QUERY result for a single pr summary."
+  (let* ((data (gethash "pullRequest"
+                        (gethash "repository"
+                                 (gethash "data" query))))
+         (title (gethash "title" data))
+         (body (gethash "body" data))
+         (branch-name (gethash "headRefName" data))
+         (commit (gethash "headRefOid" data))
+         (author (gethash "login" (gethash "author" data)))
+         (files (gethash "edges" (gethash "files" data))))
+    (list
+     commit
+     branch-name
+     author
+     title
+     body
+     (seq-map (lambda (file)
+                (let ((data (gethash "node" file)))
+                  (list (gethash "path" data)
+                        (gethash "additions" data)
+                        (gethash "deletions" data))))
+              files))))
 
 (defun gh--filter-labels (labels)
   "Remove merge-queue and size from LABELS."
@@ -168,12 +187,12 @@
 (defun gh--load-prs ()
   "Query the graphql api for pr data."
   (let ((json-data (gh--load (gh-prs-query))))
-    (gh--parse-pr-query json-data)))
+    (gh--parse-all-prs-query json-data)))
 
 (defun gh--load-pr (number)
   "Query the graphql api for an overview of a specific pr NUMBER."
   (let ((json-data (gh--load (gh-pr-query number))))
-    json-data))
+    (gh--parse-single-pr-query json-data)))
 
 (defun gh--format-pr-title (title author)
   "Format TITLE and AUTHOR to a suitable fixed-width format for the pr buffer."
@@ -209,6 +228,35 @@
           (format "%3s" size) " "
           (gh--format-labels labels) "\n")))
 
+(defun gh--insert-pr-summary (pr)
+  "Insert PR summary data into the current buffer."
+  (let* ((commit (car pr))
+         (crest (cdr pr))
+         (branch (car crest))
+         (brest (cdr crest))
+         (author (car brest))
+         (arest (cdr brest))
+         (title (car arest))
+         (trest (cdr arest))
+         (body (car trest))
+         (borest (cdr trest))
+         (files (car borest)))
+    (insert
+     (format "branch: %s\n" branch)
+     (format "commit: %s\n" commit)
+     (format "author: %s\n\n" author)
+     (format "%s\n\n%s\n\n" title body))
+    (seq-do (lambda (file)
+              (insert (gh--summarize-file file)))
+            files)))
+
+(defun gh--summarize-file (file)
+  "Generate a string summarizing FILE, similar to git diff --stat."
+  (let ((path (car file))
+        (additions (cadr file))
+        (deletions (caddr file)))
+    (format "%-80s | +%5d -%5d\n" path additions deletions)))
+
 (defun gh-refresh-buffer ()
   "Refresh the github data in the current buffer."
   (interactive)
@@ -232,12 +280,26 @@
                           (not (equal gh-user (caddr pr))))
                         pr-data))))
 
+(defun gh--refresh-pr (number)
+  "Refresh the current buffer with a summary of pr NUMBER."
+  (erase-buffer)
+  (let ((pr-data (gh--load-pr number)))
+    (gh--insert-pr-summary pr-data)))
+
 (defun gh-open-buffer ()
   "Open a new github-mode buffer."
   (interactive)
   (let ((buffer (get-buffer-create "*github-prs*")))
     (switch-to-buffer buffer)
     (gh--refresh-prs)))
+
+(defun gh-open-pr-summary (pr-number)
+  "Open a summary of PR-NUMBER in a new buffer."
+  (interactive "npr number: ")
+  (let* ((pr-buffer-name (format "*github-pr-%d*" pr-number))
+         (buffer (get-buffer-create pr-buffer-name)))
+    (switch-to-buffer buffer)
+    (gh--refresh-pr pr-number)))
 
 (provide 'github)
 
